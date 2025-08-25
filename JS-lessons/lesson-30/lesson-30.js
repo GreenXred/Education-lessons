58970970f1f8cb5a98a31b26afc28a28a80f0b5f
GreenXred <antonzo09@mail.ru>
2025-08-25 23:07:16 +0800

    Task 30 completed

--- JS-lessons/lesson-30/lesson-30.js
+++ JS-lessons/lesson-30/lesson-30.js
@@ -1,28 +1,34 @@
-//Задание 1.
-
-// document.getElementById('div1').addEventListener('click', () => {
-//     console.log('Это div1');
-// });
-
-// document.getElementById('div2').addEventListener('click', () => {
-//     console.log('Это div2');
-// });
-
-// document.getElementById('div3').addEventListener('click', () => {
-//     console.log('Это div3');
-// });
-
-// Задание 2.
+// 1. Создай три вложенных элемента (например, `div` внутри `div` внутри `div`). 
+// Назначь обработчики событий для каждого из них и проследи за последовательностью вызовов при клике на внутренний элемент с помощью console.log();
 
 document.getElementById('div1').addEventListener('click', () => {
-    console.log('Это div1');
+    console.log('div1 clicked');
 });
-
 document.getElementById('div2').addEventListener('click', () => {
-    console.log('Это div2');
+    console.log('div2 clicked');
+});
+document.getElementById('div3').addEventListener('click', () => {
+    console.log('div3 clicked');
 });
 
-document.getElementById('div3').addEventListener('click', (event) => {
-    console.log('Это div3');
-    event.stopImmediatePropagation();
+//2. Напиши код, который останавливает всплытие события на среднем элементе из предыдущего задания;
+
+document.getElementById('div2').addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+//3. Создай форму с несколькими полями ввода и кнопкой отправки. 
+// Реализуй делегирование события, например, валидации каждого поля ввода при его изменении.
+// Пусть это будет простое условие, например, длина строки не более 20 символов.
+
+const form = document.getElementById('validationForm')
+const maxInputLength = 20;
+
+form.addEventListener('input', (event) => {
+  const currentInput = event.target;
+  if (currentInput.classList.contains('validate')) {
+    if (currentInput.value.length > maxInputLength) {
+        currentInput.value = currentInput.value.slice(0, maxInputLength); //принимает только первые 20 символов
+    }
+  }
 });
