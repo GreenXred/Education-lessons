aba4fc90f70c3c552a8256e1a7582d512a9bb3b7
GreenXred <antonzo09@mail.ru>
2025-09-08 22:54:49 +0800

    Task 5 completed

--- REACT-lessons/src/main.jsx
+++ REACT-lessons/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './lesson-4/lesson-4.jsx'
+import App from './lesson-5/lesson-5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
