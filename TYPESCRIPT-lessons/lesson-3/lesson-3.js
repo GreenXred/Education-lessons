21a0c95af6e57795a5bde5f19b0f71f226209f86
GreenXred <antonzo09@mail.ru>
2025-10-31 17:52:20 +0300

    Исправления

--- TYPESCRIPT-lessons/lesson-3/lesson-3.js
+++ TYPESCRIPT-lessons/lesson-3/lesson-3.js
@@ -2,7 +2,7 @@
 // Используя пересечение, создайте тип `FeaturedProduct` и напиши функцию, принимающую этот тип и выводящую информацию о продукте со скидкой;
 function displayFeaturedProduct(product) {
     var discountedPrice = product.price * (1 - product.discount / 100);
-    console.log("Product: ".concat(product.name, " - ").concat(discountedPrice.toFixed(), " (Discount: ").concat(product.discount, "%)"));
+    console.log("Product: ".concat(product.name, " - ").concat(discountedPrice.toFixed(2), " (Discount: ").concat(product.discount, "%)"));
 }
 var featuredProduct = {
     name: "Iphone",
@@ -13,9 +13,12 @@ displayFeaturedProduct(featuredProduct);
 // 2. Создай переменную типа `unknown` и напиши функцию, которая принимает эту переменную, и обрабатывает её в зависимости от типа. 
 // Например, если тип переменной - строка, то выводим ее в консоль и так далее;
 var data = "some string";
-if (typeof data === 'string') {
-    console.log(data.toUpperCase());
+function handleUnknown(data) {
+    if (typeof data === 'string') {
+        console.log(data.toUpperCase());
+    }
 }
+handleUnknown(data);
 function throwRoleError(role) {
     throw new Error("\u041D\u0435\u0432\u0435\u0440\u043D\u044B\u0439 \u0442\u0438\u043F \u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044F: ".concat(role));
 }
@@ -38,10 +41,7 @@ function filterTasks(tasks, person) {
     else if (person.role === 'user') {
         return tasks.filter(function (task) { return task.isActive; });
     }
-    else if (person.role === 'guest') {
-        return tasks.filter(function (task) { return task.isPublic; });
-    }
-    return [];
+    return tasks.filter(function (task) { return task.isPublic; }); // guest
 }
 var tasks = [
     { title: "Task 1", isActive: true, isPublic: true, isHidden: false },
